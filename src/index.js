f0a2cec02012a8b62f1d7802d6c5ef9e8269f2ec
Aspen Wilson <aspen.schell@gmail.com>
2023-01-26 12:48:05 -0800

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskList = document.querySelector('form')
+  taskList.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToList(e.target.new_task_description.value)
+    taskList.reset()
+  })
 });
+
+function addToList (task) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteButton)
+  btn.textContent = 'Done!'
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function deleteButton (e) {
+  e.target.parentNode.remove()
+}
+
+
+//As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
